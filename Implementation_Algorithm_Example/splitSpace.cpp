2c31fdb6225d06a55e6455dde4f9954ae167fe7c
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2022-09-30 02:04:16 +0900

    Update splitSpace.cpp

--- Implementation_Algorithm_Example/splitSpace.cpp
+++ Implementation_Algorithm_Example/splitSpace.cpp
@@ -1,1 +1,20 @@
-
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+vector<string> splitSpace(string &s) {
+	vector<string> v;
+	string temp;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ') {
+			v.push_back(temp);
+			temp.clear();
+		}
+		else {
+			temp.push_back(s[i]);
+		}
+	}
+	
+	v.push_back(temp); // 맨 마지막 
+	return v;
+}
